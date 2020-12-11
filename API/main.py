f67b4ce31bed64e922ac4340d993aa1d839f3609
Aditya-2001 <iit2019210@iiita.ac.in>
2020-12-11 16:41:07 +0530

    Deleted the user pass for company

--- API/main.py
+++ API/main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,jsonify
+from flask import Flask,render_template,request,redirect,jsonify,session,g
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy.exc import IntegrityError
@@ -11,16 +11,25 @@ from random import *
 app = FlaskAPI(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post.db'
 db=SQLAlchemy(app)
+app.secret_key = 'akadbakadbabmbebosoomailagalulubyebye'
 
 mail = Mail(app)  
 app.config["MAIL_SERVER"]='smtp.gmail.com'  
 app.config["MAIL_PORT"] = 465      
-app.config["MAIL_USERNAME"] = ''  
-app.config['MAIL_PASSWORD'] = ''  
+app.config["MAIL_USERNAME"] = 'djangonotification@gmail.com'  
+app.config['MAIL_PASSWORD'] = 'aditya.iiita0330'  
 app.config['MAIL_USE_TLS'] = False  
 app.config['MAIL_USE_SSL'] = True  
 
 mail = Mail(app)  
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        users=User.query.all()
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
 
 class User(db.Model):
     id=db.Column(db.Integer, primary_key=True, unique=True)
@@ -32,11 +41,21 @@ class User(db.Model):
     Profession=db.Column(db.String(50), nullable=False)
     Role=db.Column(db.String(50), nullable=False)
     Contact=db.Column(db.Integer, nullable=False)
-    CompanyName=db.Column(db.String(50), nullable=False)
-    CompanyPassword=db.Column(db.String(50), nullable=False)
     datePosted=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    authenticated = db.Column(db.Boolean, default=False)
     def __repr__(self):
         return 'User ' + str(self.id)
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return self.email
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_anonymous(self):
+        return False
 
 class UserSchema(Schema):
     id = fields.Int(dump_only=True)
@@ -49,8 +68,7 @@ class UserSchema(Schema):
     Role=fields.Str()
     Contact=fields.Int()
     datePosted=fields.DateTime(dump_only=True)
-    CompanyName=fields.Str()
-    CompanyPassword=fields.Str()
+    authenticated=fields.Bool()
     Name = fields.Method("format_name", dump_only=True)
 
     def format_name(self, author):
@@ -74,7 +92,7 @@ def signup1():
         Profession=request.form['profession']
         Role=request.form['role']
         Contact=request.form['contact']
-        post=User(Email=Email, Password=Password, FirstName=fName, LastName=lName, Gender=Gender, Profession=Profession, Role=Role, Contact=Contact, CompanyName='Aditya', CompanyPassword='Aditya')
+        post=User(Email=Email, Password=Password, FirstName=fName, LastName=lName, Gender=Gender, Profession=Profession, Role=Role, Contact=Contact)
         db.session.add(post)
         try:
             db.session.commit()
@@ -87,7 +105,7 @@ def signup1():
         return "Invalid request"
 
 json = FlaskJSON(app)
-@app.route("/user/signup/<name>",methods=['POST'])
+@app.route("/user/signup",methods=['POST'])
 def signup(name):
     data = request.get_json(force=True)
     try:
@@ -99,9 +117,7 @@ def signup(name):
         Profession=data['profession']
         Role=data['role']
         Contact=int(data['contact'])
-        CompanyPassword=data['company_password']
-        CompanyName=name
-        post=User(Email=Email, Password=Password, FirstName=fName, LastName=lName, Gender=Gender, Profession=Profession, Role=Role, Contact=Contact, CompanyName=CompanyName, CompanyPassword=CompanyPassword)
+        post=User(Email=Email, Password=Password, FirstName=fName, LastName=lName, Gender=Gender, Profession=Profession, Role=Role, Contact=Contact)
         db.session.add(post)
         try:
             db.session.commit()
@@ -113,20 +129,31 @@ def signup(name):
         raise JsonError(description='Invalid value/ Email already registered')
     return json_response(Email=Email, FirstName=fName, LastName=lName, Gender=Gender, Profession=Profession, Role=Role, Contact=Contact, CompanyName=CompanyName)
 
-@app.route("/user/all/<name>/<password>",methods=['GET','POST'])
-def allusers(name,password):
-    users=User.query.filter_by(CompanyName=name, CompanyPassword=password)
+@app.route("/user/all",methods=['GET','POST'])
+def allusers():
+    users=User.query.filter_by()
     result=users_schema.dump(users)
     return json_response(Users=result)
+
 
 @app.route("/user/login1",methods=['POST'])
 def login1():
     if request.method=="POST":
+        session.pop('user_id', None)
         Email=request.form['email']
         Password=request.form['password']
         post=User.query.filter_by(Email=Email, Password=Password).first()
         try:
             ID=post.id
+            post.authenticated=True
+            session['user_id'] = post.id
+            db.session.add(post)
+            try:
+                db.session.commit()
+            except:
+                db.session.rollback()
+            finally:
+                db.session.close()
             return "Login Success"
         except:
             return "Login Failed"
@@ -136,18 +163,47 @@ def login1():
 @app.route("/user/login",methods=['POST'])
 def login():
     data = request.get_json(force=True)
+    session.pop('user_id', None)
     try:
         Email=data['email']
         Password=data['password']
         post=User.query.filter_by(Email=Email, Password=Password).first()
         try:
             ID=post.id
-            return json_response(Email=Email)
+            post.authenticated=True
+            session['user_id'] = post.id
+            db.session.add(post)
+            try:
+                db.session.commit()
+            except:
+                db.session.rollback()
+            finally:
+                db.session.close()
+            return json_response(Email=Email, Authenticated=True)
         except:
             pass
     except (KeyError, TypeError, ValueError):
         raise JsonError(description='Invalid value')
     return json_response(status=400)
+
+@app.route("/user/logout",methods=['POST','GET'])
+def logout():
+    print(g.user)
+    if g.user == None:
+        return json_response(status=400, Description="Already Logged out")
+    user_id=g.user.id
+    g.user.authenticated=False
+    db.session.add(g.user)
+    try:
+        db.session.commit()
+        session.pop('user_id', None)
+        return json_response(status=200, Authenticated=False)
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return json_response(status=400)
+
 
 @app.route("/user/EmailVerification1",methods=['POST'])
 def emailVerify1():
@@ -167,7 +223,7 @@ def emailVerify(message):
 
 def otpSend(message,email):
     otp = randint(000000,999999)
-    msg = Message('OTP',sender = '', recipients = [email])  
+    msg = Message('OTP',sender = 'djangonotification@gmail.com', recipients = [email])  
     msg.body = 'OTP for ' + str(message) + ' is: ' + str(otp)  
     mail.send(msg) 
     return str(otp)
@@ -176,7 +232,7 @@ def otpSend(message,email):
 def resetPassword1():
     Email=request.form['email']
     NewPassword=request.form['new_password']
-    post=User.query.filter_by(Email=Email, CompanyName='Aditya', CompanyPassword='Aditya').first()
+    post=User.query.filter_by(Email=Email).first()
     try:
         post.Password=NewPassword
         try:
@@ -196,9 +252,7 @@ def resetPassword():
     try:
         Email=data['email']
         NewPassword=data['new_password']
-        CompanyName=data['company_name']
-        CompanyPassword=data['company_password']
-        post=User.query.filter_by(Email=Email, CompanyName=CompanyName, CompanyPassword=CompanyPassword).first()
+        post=User.query.filter_by(Email=Email).first()
         try:
             post.Password=NewPassword
             try:
